48a2e8261fe5d6d9f55a4d9804ddb9f5a203654c
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-02 12:42:40 +0200

    sanitisation: Implemented log info value sanitisation

--- NuciLog.Core/LogMessageBuilder.cs
+++ NuciLog.Core/LogMessageBuilder.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace NuciLog.Core
 {
-    public static class LogMessageBuilder
+    public static partial class LogMessageBuilder
     {
+        private static readonly Regex NewLineMatchingRegex = new(@"\r\n|\r|\n", RegexOptions.Compiled);
+
         public static string Build(
             Operation operation,
             OperationStatus operationStatus,
@@ -57,29 +60,68 @@ namespace NuciLog.Core
 
             if (!string.IsNullOrWhiteSpace(message))
             {
-                processedLogInfos.Add(new LogInfo(LogInfoKey.Message, message));
+                processedLogInfos.Add(new(
+                    LogInfoKey.Message,
+                    SanitiseLogInfoValue(message)));
             }
             else if (ex is not null)
             {
-                processedLogInfos.Add(new LogInfo(LogInfoKey.Message, "An exception has occurred"));
+                processedLogInfos.Add(new(
+                    LogInfoKey.Message,
+                    "An exception has occurred"));
             }
 
             if (logInfos is not null)
             {
-                processedLogInfos.AddRange(logInfos);
+                foreach (LogInfo logInfo in logInfos)
+                {
+                    if (logInfo.Value is string stringValue)
+                    {
+                        processedLogInfos.Add(new(
+                            logInfo.Key,
+                            SanitiseLogInfoValue(stringValue)));
+                    }
+                    else
+                    {
+                        processedLogInfos.Add(logInfo);
+                    }
+
+                    processedLogInfos.Add(logInfo);
+                }
             }
 
             if (ex is not null)
             {
-                processedLogInfos.Add(new LogInfo(LogInfoKey.Exception, ex.GetType()));
-                processedLogInfos.Add(new LogInfo(LogInfoKey.ExceptionMessage, ex.Message));
-                processedLogInfos.Add(new LogInfo(LogInfoKey.StackTrace, ex.StackTrace));
+                processedLogInfos.Add(new(
+                    LogInfoKey.Exception,
+                    ex.GetType()));
+                processedLogInfos.Add(new(
+                    LogInfoKey.ExceptionMessage,
+                    SanitiseLogInfoValue(ex.Message)));
+                processedLogInfos.Add(new(
+                    LogInfoKey.StackTrace,
+                    SanitiseLogInfoValue(ex.StackTrace)));
             }
 
             return processedLogInfos
                 .GroupBy(x => x.Key)
                 .Select(g => new LogInfo(g.First().Key, g.Last().Value))
                 .Where(x => !string.IsNullOrWhiteSpace(x.Value));
+        }
+
+        private static string SanitiseLogInfoValue(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return value;
+            }
+
+            string sanitisedValue = value;
+
+            sanitisedValue = NewLineMatchingRegex.Replace(sanitisedValue, "\\n");
+            sanitisedValue = sanitisedValue.Replace(",", "͵");
+
+            return sanitisedValue;
         }
     }
 }
