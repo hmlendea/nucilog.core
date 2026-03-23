4d376700c3996a9b3c33a99e73af45898390e30c
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-23 13:04:05 +0200

    null-logger: Added a null logger

---
+++ NuciLog.Core/NullLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NuciLog.Core
+{
+    public sealed class NullLogger : Logger
+    {
+        protected override void WriteLog(
+            LogLevel level,
+            Func<string> logMessage)
+        { }
+    }
+}
